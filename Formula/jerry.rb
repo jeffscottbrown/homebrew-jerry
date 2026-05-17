ea3c547556e56901e16ebd2f5ab20ee7bde1c8fa
Jeff Brown <brownj@objectcomputing.com>
2026-05-16 22:32:23 -0500

    setting up brew tap for jerry

---
+++ Formula/jerry.rb
@@ -0,0 +1,25 @@
+class Jerry < Formula
+    desc "Jerry programming language compiler"
+    homepage "https://github.com/jeffscottbrown/jerry-lang"
+    url "https://github.com/jeffscottbrown/jerry-lang/archive/refs/tags/v0.0.3.tar.gz"
+    sha256 "PLACEHOLDER"
+    license "MIT"
+    head "https://github.com/jeffscottbrown/jerry-lang.git", branch: "main"
+
+    depends_on "go" => :build
+
+    def install
+      system "go", "build",
+        *std_go_args(ldflags: "-s -w -X main.Version=#{version}"),
+        "./cmd/jerry"
+    end
+
+    test do
+      (testpath/"hello.jer").write <<~EOS
+        fn main() {
+          print("Hello from Homebrew!");
+        }
+      EOS
+      assert_match "Hello from Homebrew!", shell_output("#{bin}/jerry run hello.jer")
+    end
+  end
